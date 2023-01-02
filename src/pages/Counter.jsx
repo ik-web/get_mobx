7a6cb3db6a977deb5677419828a6d38a70308659
Ihor Kuchin <vedunigor@gmail.com>
2023-01-02 21:08:46 +0200

    Add counter page

---
+++ src/pages/Counter.jsx
@@ -0,0 +1,31 @@
+import { observer } from "mobx-react-lite";
+import React from "react";
+import counter from "../store/counter";
+
+const Counter = () => {
+  return (
+    <div className="counter">
+      <h1 className="counter__count">
+        <span>Count:</span>
+        <span>{counter.count}</span>
+      </h1>
+
+      <div className="counter__buttons">
+        <button className="counter__button" onClick={() => counter.increment()}>+1</button>
+        <button className="counter__button" onClick={() => counter.decrement()}>-1</button>
+      </div>
+
+      <div className="counter__buttons">
+        <button className="counter__button" onClick={() => counter.plusTen()}>+10</button>
+        <button className="counter__button" onClick={() => counter.minusTen()}>-10</button>
+      </div>
+
+      <div className="counter__buttons">
+        <button className="counter__button" onClick={() => counter.plusHundred()}>+100</button>
+        <button className="counter__button" onClick={() => counter.minusHundred()}>-100</button>
+      </div>
+    </div>
+  );
+};
+
+export default observer(Counter);
---
+++ src/store/counter.js
@@ -0,0 +1,41 @@
+import { makeAutoObservable } from "mobx";
+
+class Counter {
+  count = 0;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  increment() {
+    this.count += 1;
+  }
+
+  decrement() {
+    if (this.count > 0) {
+      this.count -= 1;
+    }
+  }
+
+  plusTen() {
+    this.count += 10;
+  }
+
+  minusTen() {
+    if (this.count > 10) {
+      this.count -= 10;
+    }
+  }
+
+  plusHundred() {
+    this.count += 100;
+  }
+
+  minusHundred() {
+    if (this.count > 100) {
+      this.count -= 100;
+    }
+  }
+}
+
+export default new Counter();
